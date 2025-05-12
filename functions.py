3f9faa29a333c7f76e512f8ce842fcbfa86bcbf6
Pedro <pedronas1201@gmail.com>
2025-05-12 11:16:01 -0300

    Capturando top 50 mais vendidos globalmente

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def check_if_available(reason:  str) -> str:
     """
     Checa o status de lançamento do jogo.
@@ -15,3 +18,34 @@ def check_if_available(reason:  str) -> str:
         return 'Early Access'
     elif reason.startswith('Pre-Purchase'):
         return 'Pre-Order'
+    
+def baixar_pagina(url : str) -> str:
+    """
+    Retorna o conteúdo html da página que se deseja baixar.
+
+    Args:
+        url (str): url da página sendo baixada.
+
+    Returns:
+        url.text (str): código html da página baixada.
+    """
+
+    url = requests.get(url)
+    url.encoding = "utf-8"
+    return url.text
+
+def extrair_genero(pagina : str, lista_generos : list):
+    """
+    Devolve todos os gêneros associados ao jogo mostrado na página
+
+    Args:
+        pagina (str): conteúdo html da página do jogo.
+
+    Returns:
+        lista_generos (list): lista com todos os gêneros associados ao jogo.
+    """
+
+    soup = BeautifulSoup(pagina, "html.parser")
+    for tag in soup.find_all("a", class_="app_tag"):
+        lista_generos.append(tag.text.strip("\n\t"))
+    return lista_generos
